03d057d1d54ca29e929a254dff3d7dc63932d14d
AnzoDK <antonr@live.dk>
2020-03-09 14:29:53 +0100

    Created Room generation (Not very efficient)

--- Source/dungeon.cpp
+++ Source/dungeon.cpp
@@ -1,25 +1,139 @@
 #include "dungeon.h"
 #include <algorithm>
 #include <random>
+#include "dice.h"
+#include "game.h"
 RoomConnector::RoomConnector()
 {
-    right = 0;
-    left = 0;
-    forward = 0;
-    backwards = 0;
+    /*
+    right = -1;
+    left = -1;
+    forward = -1;
+    backwards = -1;
+    */
+   r_right = Room();
+   r_left = Room();
+   r_forward = Room();
+   r_backwards = Room();
 }
+
+RoomEvent::RoomEvent()
+{
+    options = "";
+    eventName = "None";
+    Init();
+}
+int RoomEvent::Event()
+{
+    //this is the None-Event...
+}
+void RoomEvent::useOptions(std::string input, Character* c)
+{
+    //options are not available for the None-Event;
+}
+std::string RoomEvent::getEventName()
+{
+    return eventName;
+}
+
+void Ambush::Init()
+{
+    eventName = "Ambush";
+    options = "attempt escape, fight";
+}
+int Ambush::Event()
+{
+    std::cout << "You got ambushed! - Monsters are closing in on you..." << std::endl << "You got new options! : " << "[" << options << "]" << std::endl;
+    return 0;
+}
+void Ambush::useOptions(std::string input, Character* c)
+{
+    if(input.find("attempt") != std::string::npos)
+    {
+        int roll = Dice::iRoll(20);
+        if(roll + c->currentStats.Dex > 17)
+        {
+            //Sucess
+            std::cout << "You escaped the monsters!" << std::endl << "You go back a room" << std::endl;
+            Game::nextDirection = "backwards";
+        }
+        else
+        {
+            //fail
+
+            //Combat!
+        }
+        
+    }
+    else if(input.find("fight") != std::string::npos)
+    {
+        //combat
+    }
+    else
+    {
+        //invalid Input
+        std::cout << "Invalid Input" << std::endl;
+        std::string inp = "";
+        getline(std::cin,inp);
+        useOptions(inp,c);
+    }
+    
+}
+
 
 Room::Room(RoomType T)
 {
     rt = T;
     rc = RoomConnector();
+    re = RoomEvent();
 }
 RoomType Room::getRoomType()
 {
     return rt;
 }
 
-void Dungeon::GenerateLayout(int entryID,int exitID,Floor f,std::default_random_engine rng)
+Room Dungeon::GenerateRoom()
+{
+    Room tmpRoom = Room(static_cast<RoomType>(Dice::iRoll(3)-1));
+    if(Dice::iRoll(100) > 70)
+    {
+        switch(Dice::iRoll(2))
+        {
+            case 1:
+                tmpRoom.re = Ambush();
+            break;
+            case 2:
+                tmpRoom.re = RoomEvent();
+            break;
+        }
+    }
+    else
+    {
+        tmpRoom.re = RoomEvent();
+    }
+    return tmpRoom;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*void Dungeon::GenerateLayout(int entryID,int exitID,Floor f,std::default_random_engine rng)
 {
     int seed = rand() % 10000 + 1;
     rng.seed = seed;
@@ -155,32 +269,72 @@ void Dungeon::GenerateLayout(int entryID,int exitID,Floor f,std::default_random_
     }
 
 }
+*/
 
-void Dungeon::GenerateRooms()
+
+/*int Dungeon::GenerateRoom(std::vector<Room> *rs,int *createCounter)
+{
+    int counter = int(*createCounter);
+    while(counter > 0)
+    {
+        Room currTmpRoom = Room(static_cast<RoomType>(rand() % 2));
+        switch(currTmpRoom.getRoomType())
+        {
+            case RoomType::normal:
+
+            break;
+        }
+        counter--;
+    }
+}
+*/
+/*void Dungeon::GenerateRooms()
 {
     
-    std::vector<Room> rooms = std::vector<Room>();
+    std::vector<Room> *rooms = new std::vector<Room>();
     Room entryRoom = Room(static_cast<RoomType>(4));
-    rooms.push_back(entryRoom);
+    //rooms.push_back(entryRoom);
     long roomCount = rand() %10000 +1;
     std::cout << "Generating " << roomCount << " Rooms - Hold on!" << std::endl;
-    std::vector<long>Trooms = std::vector<long>();
-    std::vector<long>crossRooms = std::vector<long>();
+    std::vector<int>Trooms = std::vector<int>();
+    std::vector<int>crossRooms = std::vector<int>();
+    int roomsToCreate = roomCount;
     for(int i = 0;i < roomCount;i++)
     {
-        Room room;
-        int type = rand() %2;
-        room = Room(static_cast<RoomType>(type));
-        room.id = i;
-        if(type == 1)
+        Room currTmpRoom = Room(static_cast<RoomType>(rand() % 2));
+        currTmpRoom.id = i;
+        if(i = 0)
         {
-            Trooms.push_back(room.id);
+            //This is the first room
+            //currTmpRoom.rc = RoomConnector();
+            currTmpRoom = Room(RoomType::entry);
+            currTmpRoom.rc.backwards = -1;
+            currTmpRoom.id = i;
+            rooms->push_back(currTmpRoom);
+            Room roomTwo = Room(RoomType::normal);
+            roomTwo.id = i+1;
+            roomTwo.rc.backwards = i;
+            rooms->push_back(roomTwo);
+            
         }
-        else if (type == 2)
+        else if(i = roomCount-1)
         {
-            crossRooms.push_back(room.id);
+            //this is the last room
+            //currTmpRoom.rc = RoomConnector();
+            currTmpRoom = Room(RoomType::normal);
+            currTmpRoom.rc.forward = i+1;
+            currTmpRoom.id = i;
+            Room endRoom = Room(RoomType::exit);
+            endRoom.id = i+1;
+            endRoom.rc.backwards = i;
+            endRoom.rc.forward = -1;
+            
         }
-        rooms.push_back(room);
+        else
+        {
+            GenerateRoom(rooms,&roomsToCreate);
+        }
+        
 
     }
     Room exitRoom = Room(static_cast<RoomType>(5));
@@ -189,6 +343,9 @@ void Dungeon::GenerateRooms()
     f.floorRooms = rooms;
     floors.push_back(f);
 }
+*/
+
+/*
 void Dungeon::GenerateFloors()
 {
     auto rng = std::default_random_engine {};
@@ -200,3 +357,4 @@ void Dungeon::GenerateFloors()
         GenerateLayout(0,floors.at(i).floorRooms.size()-1,floors.at(i),rng);
     }
 }
+*/
